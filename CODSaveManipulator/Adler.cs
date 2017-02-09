96060a4e7a334c0cbf5e54fb48499c3b77890d09
Hunter Stanton <ihatecompvir@icloud.com>
2017-02-09 14:05:41 -0500

    Updated usage info

---
+++ CODSaveManipulator/Adler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+// Implements Adler32 checksumming, used in calculation of Ghosts savegame checksum
+
+namespace GhostsChecksumFixer
+{
+    class Adler
+    {
+        private static uint MOD_ADLER = 65521;
+        private uint Checksum;
+
+        public long Value
+        {
+            get
+            {
+                return (long)this.Checksum;
+            }
+        }
+
+        public Adler()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this.Checksum = 1U;
+        }
+
+        /// <summary>
+        /// Shorthand version of Update.
+        /// </summary>
+        /// <param name="buff">The buffer of data that will be checksummed.</param>
+        public void Update(byte[] buff)
+        {
+            this.Update(buff, buff.Length);
+        }
+
+        /// <summary>
+        /// Calculates Adler32 checksum of input buffer.
+        /// </summary>
+        /// <param name="buff">The buffer of data that will be checksummed.</param>
+        /// <param name="length">How much of the buffer to checksum.</param>
+        public void Update(byte[] buff, int length)
+        {
+            int offset = 0;
+
+            if (buff == null)
+            {
+                // Buffer should never be null, if is throw error
+                throw new ArgumentNullException("buf");
+            }
+
+            uint check1 = this.Checksum & (uint)ushort.MaxValue;
+            uint check2 = this.Checksum >> 16;
+            while (length > 0)
+            {
+                int check3 = 3800;
+
+                if (check3 > length)
+                {
+                    check3 = length;
+                }
+
+                length -= check3;
+
+                while (--check3 >= 0)
+                {
+                    check1 += (uint)buff[offset++] & (uint)byte.MaxValue;
+                    check2 += check1;
+                }
+
+                check1 %= Adler.MOD_ADLER;
+                check2 %= Adler.MOD_ADLER;
+            }
+            this.Checksum = check2 << 16 | check1;
+        }
+    }
+}
--- CODSaveManipulator/Program.cs
+++ CODSaveManipulator/Program.cs
@@ -10,6 +10,11 @@ namespace CODSaveManipulator
     {
         static void Main(string[] args)
         {
+            if(args.Length != 2)
+            {
+                System.Console.WriteLine("CODSaveManipulator\nA program for manipulating Call of Duty savegames in various ways.\n\nFunctions that are currently supported:\n\n-rehash\n    Fixes the checksum on a Call of Duty savegame\n    Usage: CODSavegameManipulator -rehash <savegame.svg>\n\n-info\n    Prints various information about a savegame\n    Usage: CODSavegameManipulator -info <savegame.svg>");
+                return;
+            }
         }
     }
 }
