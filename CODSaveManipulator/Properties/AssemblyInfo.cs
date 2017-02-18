06c71d3b470442c8c35c201a7c1ba4dbf41d1c1b
Hunter Stanton <ihatecompvir@icloud.com>
2017-02-18 01:01:12 -0500

    Added icon and updated assembly info

--- CODSaveManipulator/Properties/AssemblyInfo.cs
+++ CODSaveManipulator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CODSaveManipulator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A tool for manipulating saves for the popular Call of Duty series.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Hunter Stanton")]
 [assembly: AssemblyProduct("CODSaveManipulator")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Hunter Stanton 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
